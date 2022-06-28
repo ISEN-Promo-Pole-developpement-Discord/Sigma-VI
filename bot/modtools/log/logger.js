462c6b2cd57c2d9f8b89a48f1551b2d209eb61b2
Tom Bourjala <tom.bourjala@gmail.com>
2022-06-29 00:18:30 +0200

    feat: logger manager, wich can now find out wich channel is to log on

---
+++ bot/modtools/log/logger.js
@@ -0,0 +1,18 @@
+// [Guild ID] => {adminLog: [Channel ID], ioLog: [Channel ID], userLog: [Channel ID]}
+var guildLogMap = new Map();
+
+const {logAdminChannelName, logIoChannelName, logUserChannelName} = require("../../config-core.json");
+
+function getGuildLogChannel(guild, type) {
+    if (!guildLogMap.has(guild.id)) {
+        let adminLogChannel = guild.channels.find(channel => channel.name === logAdminChannelName);
+        let ioLogChannel = guild.channels.find(channel => channel.name === logIoChannelName);
+        let userLogChannel = guild.channels.find(channel => channel.name === logUserChannelName);
+        guildLogMap.set(guild.id, {admin: adminLogChannel, io: ioLogChannel, user: userLogChannel});
+    }
+    return guildLogMap.get(guild.id)[type];
+}
+
+module.exports = {
+    getGuildLogChannel
+}
