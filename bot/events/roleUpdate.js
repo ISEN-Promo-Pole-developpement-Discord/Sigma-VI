ce73badd39c9da4ba0d283137dbfcdbb10a4402e
brandy223 <brandan.legall@gmail.com>
2022-06-30 00:06:34 +0900

    feat: role update log

--- bot/events/roleUpdate.js
+++ bot/events/roleUpdate.js
@@ -1,3 +1,5 @@
+const {logAdminUpdate} = require('../modtools/log/log-admin.js');
+
 module.exports = {
     name: "roleUpdate",
     once: false,
@@ -8,6 +10,16 @@ module.exports = {
          * @param {Role} newRole The role after it was updated
          * @event roleUpdate
          * @returns {Promise<void>}
-            */
+        */
+
+        logAdminUpdate(
+            oldRole.guild,
+            `Role "${oldRole.name}" updated`,
+            null,
+            null,
+            [{name:"ID", value:oldRole.id, inline:true}, {name:"Creation", value:new Date(newRole.createdTimestamp).toLocaleString(), inline:true}],
+            oldRole.name,
+            newRole.name,
+        );
     }
 }
