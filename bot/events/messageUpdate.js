496a40064a863aaceeaf71b7678659277cb1a5ff
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-06-30 11:20:45 +0200

    feat: log for message updated

--- bot/events/messageUpdate.js
+++ bot/events/messageUpdate.js
@@ -1,3 +1,5 @@
+const {logAdminUpdate} = require('../modtools/log/log-admin.js');
+
 module.exports = {
     name: "messageUpdate",
     once: false,
@@ -8,5 +10,17 @@ module.exports = {
          * @param {Message} newMessage
          * @returns {Promise<void>}
          */
+         logAdminUpdate(
+            oldMessage.guild,
+            `Message updated`,
+            {
+                username: oldMessage.author.tag,
+                avatarURL: oldMessage.author.displayAvatarURL()
+              },
+              null,
+            [{name:"ID", value:newMessage.id, inline:true}, {name:"Send", value:`<t:${Math.floor(newMessage.createdTimestamp/1000)}:f>`, inline:true},{name:"Channel", value:`${newMessage.channel}`, inline:true}],
+            oldMessage.content,
+            newMessage.content,
+        );        
     }
 }
