057bb0cb7e9715ba4974dcc908538243424629d0
JacksonKyoshi <61598419+JacksonKyoshi@users.noreply.github.com>
2022-06-30 18:16:39 +0200

    feat: log channel update

--- bot/events/channelUpdate.js
+++ bot/events/channelUpdate.js
@@ -1,3 +1,5 @@
+const {logAdminUpdate} = require('../modtools/log/log-admin.js');
+
 module.exports = {
     name: "channelUpdate",
     once: false,
@@ -8,5 +10,20 @@ module.exports = {
          * @param {Channel} newChannel The new channel
          * @event channelUpdate
          */
+        /*
+        oldChannel.permissionOverwrites.cache.forEach((snowflake, permOverwrites) => {
+            console.log(snowflake);
+            console.log(permOverwrites);
+          });
+          */
+
+          logAdminUpdate(
+            oldChannel.guild,
+            "Channel",
+            null,
+            null,
+            oldChannel,
+            newChannel
+        );
     }
 }
