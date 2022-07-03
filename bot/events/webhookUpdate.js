6dc395c8a62087c8de81e4203d7c68a26548962c
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-07-03 23:28:48 +0200

    feat: Log for webhooks Update

--- bot/events/webhookUpdate.js
+++ bot/events/webhookUpdate.js
@@ -1,3 +1,5 @@
+const {logAdminUpdate} = require('../modtools/log/log-admin.js');
+
 module.exports = {
     name: "webhookUpdate",
     once: false,
@@ -7,5 +9,19 @@ module.exports = {
          * @param {Channel} channel The channel the webhook got updated in
          * @returns {Promise<void>}
          */
+         logAdminUpdate(
+            channel.guild,
+            "Webhook",
+            {
+                username  : channel.name,
+            },
+            null,
+            channel,
+            channel,
+        );/*
+            channel.fetchWebhooks()
+            .then(hooks => console.log(`This Channel has ${hooks.size} hook with ${hooks.webhooks} name`))
+            .catch(console.error);
+*/
     }
 }
