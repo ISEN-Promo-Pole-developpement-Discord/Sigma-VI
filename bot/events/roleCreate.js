1f5b5e81aa18ead5478e9f6505c2cc4df9917ac5
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-07-10 00:16:57 +0200

    feat: Log for the Creation of a Role

--- bot/events/roleCreate.js
+++ bot/events/roleCreate.js
@@ -1,4 +1,4 @@
-const {logAdminCreate} = require('../modtools/log/log-admin.js');
+const {logCreate} = require('../modtools/log/log-admin.js');
 
 module.exports = {
     name: "roleCreate",
@@ -9,21 +9,17 @@ module.exports = {
          * @param {Role} role The role that got created
          * @event roleCreate
          * @returns {Promise<void>}
-            */
+        */
 
         // FIXME - Update of a role provoke a role creation logs spam (in every case except one)
         // FIXME - Update of a role is considered as a role creation (in logs)
-
-        logAdminCreate(
+       
+        logCreate(
             role.guild,
-            `Role "${role.name}" created`,
+            "role",
             null,
-            null,
-            role.name,
-            role.id,
-            role.createdTimestamp,
-            `<@&${role.id}>`,
-            null
-        )
+            role,
+            "admin",
+        );
     }
 }
