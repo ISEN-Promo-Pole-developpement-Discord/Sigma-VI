b24c4fc87e16b813a251d507fa876acd573f8d85
Sigeth <52868615+Sigeth@users.noreply.github.com>
2022-07-11 12:28:40 +0200

    feat: log for creation of a stage instance

--- bot/events/stageInstanceCreate.js
+++ bot/events/stageInstanceCreate.js
@@ -1,3 +1,6 @@
+const {logCreate} = require('../modtools/log/log-admin.js');
+const {getActionAuthor} = require('../modtools/log/logger.js');
+
 module.exports = {
     name: "stageInstanceCreate",
     once: false,
@@ -7,5 +10,17 @@ module.exports = {
          * @param {StageInstance} stageInstance The stage instance that got created
          * @returns {Promise<void>}
             */
+        getActionAuthor(stageInstance.guild, stageInstance, "StageInstance").then(userAuthor => {
+            logCreate(
+                stageInstance.guild,
+                "Stage Instance",
+                {
+                    username: userAuthor.tag,
+                    avatarURL: userAuthor.displayAvatarURL(),
+                },
+                stageInstance,
+                "admin",
+            );
+        });
     }
 }
