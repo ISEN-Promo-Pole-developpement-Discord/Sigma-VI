8886bfd5bffa338afd8796dde2aea500d738c261
Sigeth <52868615+Sigeth@users.noreply.github.com>
2022-07-22 22:22:20 +0200

    feat: test for getSelectMenuFromJSON

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -1,5 +1,7 @@
 const {logCreate} = require('../modtools/log/log-admin.js');
 const {createChannel} = require('../utils/config-forms.js');
+const { getSelectMenuFromJSON } = require('../forms/formManager.js');
+const welcomeFormData = require('../forms/welcomeForm/welcomeForm.json');
 module.exports = {
     name: "guildMemberAdd",
     once: false,
@@ -11,6 +13,11 @@ module.exports = {
          * @returns {Promise<void>}
          */
          createChannel(member.guild,member);
+
+         member.createDM().then(channel => {
+            channel.send({components: getSelectMenuFromJSON(welcomeFormData, null)});
+         });
+
             logCreate(
                member.guild,
                "GuildMember",
