187b71ce9978a78919418dc7b6e88d6d64806d57
Sigeth <52868615+Sigeth@users.noreply.github.com>
2022-07-24 23:48:33 +0200

    fix: createChannel won't work

--- bot/utils/config-forms.js
+++ bot/utils/config-forms.js
@@ -1,21 +1,13 @@
+const { ChannelType } = require("discord.js");
 
 function createChannel(guild,user) {
-     guild.channels.create(`forms of user : ${user}`,{
-        type : "GUILD_TEXT" ,
-        topic : "forms for a new user",
-        nsfw : false,
-        userLimit : 10, //à changer après
-        permissionOverwrites : [{
-            id : user,
-            allow : ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-        }
-        ],
-        position : 1,
-        reason : "forms"}
-     )
-
-     .then(NewCha => {
-       NewCha.send(`bienvenue ! clique sur  : ${NewCha}`);
+     guild.channels.create(
+      {
+        name: `welcome-${user.username}-${user.discriminator}`,
+        type: ChannelType.GuildText
+      }
+     ).then(channel => {
+       channel.send(`bienvenue ! clique sur  : ${channel}`);
      })
    .catch(console.error);
 
