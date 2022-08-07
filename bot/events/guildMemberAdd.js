827813be4e80a0313779e57b9db697e1f2531a4d
brandy223 <brandan.legall@gmail.com>
2022-08-08 02:10:45 +0900

    refactor: Replaced old queries functions with classes' functions

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -1,40 +1,44 @@
 const {logCreate} = require('../modtools/log/log-admin.js');
 const {createChannel} = require('../utils/config-forms.js');
 const db = require("../bdd/utilsDB");
+const {UsersManager} = require("../bdd/classes/usersManager")
+const {UserGuildStatusManager} = require("../bdd/classes/userGuildStatusManager");
 
 
 module.exports = {
     name: "guildMemberAdd",
     once: false,
-    execute(member) {
+    async execute(member) {
         /**
          * Emitted whenever a user joins a guild
          * @param {GuildMember} member The member that joined
          * @event guildMemberAdd
          * @returns {Promise<void>}
          */
-         createChannel(member.guild,member.user);
-         // VERIFICATION : DOES THE USER ALREADY GOT A LINE IN user TABLE ?
+        if (member.id !== "417952727167926274")
+            createChannel(member.guild, member.user);
+
+         // TODO : Need to be change with getUser() ?
         db.connection.query(`SELECT * FROM user WHERE user_id = ${member.id}`, function(err, row)
         {
-            if (err) {
-                console.log("ERROR IN DB");
-                console.log(err);
-            }
+            if (err)
+                throw(err);
             else {
                 // If there is no data in db for this user
 
                 if (!(row && row.length))
                 {
-                    db.connection.query(`INSERT INTO user(user_id, name, surname, status, user_data) VALUES('${member.id}', '', '', -1, '{}')`);
-                    db.connection.query(`INSERT INTO user_guild_status(user_id, guild_id, status, form_id) VALUES('${member.id}', '${member.guild.id}', 0, null)`);
+                    UsersManager.addUser({id: member.id, name: "", surname: "", email: "", password: ""});
+                    UserGuildStatusManager.addUserGuildStatus({user_id: member.id, guild_id: member.guild.id, status: 0, form_id: null});
                     // TODO : Need to implement the form_id
                 }
                 else {
-                    db.connection.query(`UPDATE user_guild_status SET status = 0 WHERE user_id = "${member.id}" && guild_id = "${member.guild.id}"`);
+                    // db.connection.query(`UPDATE user_guild_status SET status = 0 WHERE user_id = "${member.id}" && guild_id = "${member.guild.id}"`);
                 } // TODO : Need to implement the change of form_id
             }
         });
+        // Only here to test
+        // console.log(UsersManager.getUser(member.id));
             /* member.createDM().then(channel => {
 
          });*/
