ffc2dc4860421a354973f06f234b76ad8ba790d3
Tom Bourjala <tom.bourjala@gmail.com>
2022-08-10 23:50:07 +0200

    fix:  patched pin command invalid defer

--- bot/requests/modules/ping/command.js
+++ bot/requests/modules/ping/command.js
@@ -7,9 +7,10 @@ module.exports = {
         .setDescription('Permet de tester la connexion à une URL ou aux services de l\'ISEN.')
         .addStringOption(option => option.setName('url').setDescription('L\'URL à tester.').setRequired(false)),
     async execute(interaction){
-        interaction.deferReply();
-        var url = interaction.getOption('url');
+        var defering = interaction.deferReply();
+        var url = interaction.options.getString('url');
         var reply = await CORE(url, interaction.createdAt.getTime());
-        interaction.reply(reply);
+        await defering;
+        interaction.editReply(reply);
     }
 };
