d6699fa8571bffe14a282bcb416cb45593df61cb
Tom Bourjala <tom.bourjala@gmail.com>
2022-08-10 22:19:20 +0200

    fix: removed template module

--- bot/requests/modules/template/mod.js
+++
@@ -1,9 +0,0 @@
-const {Request} = require("../../requestClass");
-
-function moduleProcess(request){
-    request.notifyStart();
-    console.log("[TEMPLATE REQUEST] "+request.message.author.tag+" : "+request.message.content);
-    request.notifyEnd("Requête traitée.\n> " + request.content + "\n> " + request.SRWF());
-}
-
-module.exports = moduleProcess;
