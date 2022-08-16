ac3f58558108ac9c6c158ff9119a6c37de2d1724
brandy223 <brandan.legall@gmail.com>
2022-08-16 20:52:14 +0900

    refactor: add status and form_id variables to constructor fix: Change of "&&" with "AND" in queries

--- bot/bdd/classes/userGuildStatus.js
+++ bot/bdd/classes/userGuildStatus.js
@@ -1,17 +1,19 @@
 class UserGuildStatus
 {
-    constructor(user_id, guild_id)
+    constructor(user_id, guild_id, status, form_id)
     {
         this.user_id = user_id;
         this.guild_id = guild_id;
+        this.status = status;
+        this.form_id = form_id;
     }
 
     // GETTERS
     async getStatus()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
-            "SELECT status FROM user_guild_status WHERE user_id = ? && guild_id = ?", [this.user_id], [this.guild_id]
+        const [rows] = await connection(
+            "SELECT status FROM user_guild_status WHERE user_id = ? AND guild_id = ?", [this.user_id], [this.guild_id]
         );
         return rows[0].status;
     }
@@ -19,23 +21,23 @@ class UserGuildStatus
     async getFormID()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
-            "SELECT form_id FROM user_guild_status WHERE user_id = ? && guild_id = ?", [this.user_id], [this.guild_id]
+        const [rows] = await connection(
+            "SELECT form_id FROM user_guild_status WHERE user_id = ? AND guild_id = ?", [this.user_id], [this.guild_id]
         );
         return rows[0].name;
     }
 
     // SETTERS
-    setStatus()
+    setStatus(status)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user_guild_status SET status = ? WHERE user_id = ? && guild_id = ?, status, [this.user_id], [this.guild_id]`)
+        connection(`UPDATE user_guild_status SET status = ? WHERE user_id = ? AND guild_id = ?`, status, [this.user_id], [this.guild_id]);
     }
 
-    setFormID()
+    setFormID(form_id)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user_guild_status SET form_id = ? WHERE user_id = ? && guild_id = ?, form_id, [this.user_id], [this.guild_id]`)
+        connection(`UPDATE user_guild_status SET form_id = ? WHERE user_id = ? AND guild_id = ?`, form_id, [this.user_id], [this.guild_id]);
     }
 }
 
