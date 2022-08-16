57b3595aa1b53842203016ac683a51e44feb54d4
brandy223 <brandan.legall@gmail.com>
2022-08-16 20:53:25 +0900

    refactor: add all the variables of users' table in constructor

--- bot/bdd/classes/user.js
+++ bot/bdd/classes/user.js
@@ -1,14 +1,20 @@
 class User
 {
-    constructor(id) {
+    constructor(id, name, surname, email, password, status, user_data) {
         this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.user_data = user_data;
     }
 
     // GETTERS
     async getName()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
+        const [rows] = await connection(
             "SELECT name FROM user WHERE id = ?", [this.id]
         );
         return rows[0].name;
@@ -17,7 +23,7 @@ class User
     async getSurname()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
+        const [rows] = await connection(
             "SELECT surname FROM user WHERE id = ?", [this.id]
         );
         return rows[0].surname;
@@ -26,7 +32,7 @@ class User
     async getEmail()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
+        const [rows] = await connection(
             "SELECT email FROM user WHERE id = ?", [this.id]
         );
         return rows[0].email;
@@ -35,7 +41,7 @@ class User
     async getStatus()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
+        const [rows] = await connection(
             "SELECT email FROM user WHERE id = ?", [this.id]
         );
         return rows[0].status;
@@ -44,7 +50,7 @@ class User
     async getData()
     {
         const connection = global.sqlConnection;
-        const [rows] = await connection.execute(
+        const [rows] = await connection(
             "SELECT email FROM user WHERE id = ?", [this.id]
         );
         return rows[0].user_data;
@@ -54,31 +60,31 @@ class User
     setName(name)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user SET name = ? WHERE user_id = ?`, name, [this.id]);
+        connection(`UPDATE user SET name = ? WHERE user_id = ?`, name, [this.id]);
     }
 
     setSurname(surname)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user SET surname = ? WHERE user_id = ?`, surname, [this.id]);
+        connection(`UPDATE user SET surname = ? WHERE user_id = ?`, surname, [this.id]);
     }
 
     setEmail(email)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user SET email = ? WHERE user_id = ?`, email, [this.id]);
+        connection(`UPDATE user SET email = ? WHERE user_id = ?`, email, [this.id]);
     }
 
     setStatus(status)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user SET status = ? WHERE user_id = ?`, status, [this.id]);
+        connection(`UPDATE user SET status = ? WHERE user_id = ?`, status, [this.id]);
     }
 
     setData(data)
     {
         const connection = global.sqlConnection;
-        connection.query(`UPDATE user SET user_data = ? WHERE user_id = ?`, data, [this.id]);
+        connection(`UPDATE user SET user_data = ? WHERE user_id = ?`, data, [this.id]);
     }
 }
 
