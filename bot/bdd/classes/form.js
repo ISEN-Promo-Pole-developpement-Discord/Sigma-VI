832e324df0ad8939d2ed2cbcae827d0f6a0ecd99
brandy223 <brandan.legall@gmail.com>
2022-08-16 20:42:34 +0900

    feat: forms and formsManager classes' code

--- bot/bdd/classes/form.js
+++ bot/bdd/classes/form.js
@@ -1,6 +1,41 @@
 class Form
 {
+    constructor(user_id, guild_id, channel_id, status) {
+        this.user_id = user_id;
+        this.guild_id = guild_id;
+        this.channel_id = channel_id;
+        this.status = status;
+    }
 
+    async getID()
+    {
+        const connection = global.sqlConnection;
+        const [rows] = await connection(
+            "SELECT form_id FROM form WHERE user_id = ? AND guild_id = ?", [this.user_id], [this.guild_id]
+        );
+        return rows[0].name;
+    }
+
+    async getStatus()
+    {
+        const connection = global.sqlConnection;
+        const [rows] = await connection(
+            "SELECT status FROM form WHERE user_id = ? AND guild_id  = ?", [this.user_id], [this.guild_id]
+        );
+        return rows[0].name;
+    }
+
+    setChannelID(channel_id)
+    {
+        const connection = global.sqlConnection;
+        connection(`UPDATE form SET channel_id = ? WHERE user_id = ? AND guild_id = ?`, channel_id, [this.user_id], [this.guild_id]);
+    }
+
+    setStatus(status)
+    {
+        const connection = global.sqlConnection;
+        connection(`UPDATE form SET status = ? WHERE user_id = ? AND guild_id = ?`, status, [this.user_id], [this.guild_id]);
+    }
 }
 
 module.exports = {Form};
--- bot/bdd/classes/formsManager.js
+++ bot/bdd/classes/formsManager.js
@@ -1,8 +1,31 @@
 const {Form} = require("./form.js");
+const {User} = require("./user");
 
 class FormsManager
 {
+    static async getForm(user_id, guild_id)
+    {
+        const connection = global.sqlConnection;
+        const query = "SELECT * FROM form WHERE user_id = ? AND guild_id = ?";
+        const data = await connection(query, args.user_id, args.guild_id, (err, result) => {
+            if (err) throw err;
+            else return result;
+        });
 
+        if (data.length === 0) return null;
+        else
+            return new Form(user_id, guild_id, data[0].channel_id, data[0].status);
+    }
+
+    static async addForm(form)
+    {
+        const connection = global.sqlConnection;
+        const query = "INSERT INTO form (user_id, guild_id, channel_id, status) VALUES (?, ?, ?, ?,)";
+        const values = [form.user_id, form.guild_id, form.channel_id, form.status];
+        await connection(query, values);
+
+        return new Form(form.user_id, form.guild_id, form.channel_id, form.status);
+    }
 }
 
 module.exports = {FormsManager};
