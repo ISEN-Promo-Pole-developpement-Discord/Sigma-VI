df17b305f4ca875673216635910b09abf87168ed
brandy223 <brandan.legall@gmail.com>
2022-08-26 05:24:54 +0900

    feat: update DB queries in guildMemberAdd event

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -3,6 +3,7 @@ const {createChannel} = require('../utils/config-forms.js');
 const db = require("../bdd/utilsDB");
 const {UsersManager} = require("../bdd/classes/usersManager")
 const {UserGuildStatusManager} = require("../bdd/classes/userGuildStatusManager");
+const {FormsManager} = require("../bdd/classes/formsManager");
 
 module.exports = {
     name: "guildMemberAdd",
@@ -14,12 +15,9 @@ module.exports = {
          * @event guildMemberAdd
          * @returns {Promise<void>}
          */
-        if(!member.user.bot)
-            createChannel(member.guild, member.user);
-        // TODO : form creation
 
         console.log(`>> [+] ${member.user.tag} a rejoint ${member.guild.name}`);
-        
+
         logCreate(
             member.guild,
             "GuildMember",
@@ -28,19 +26,33 @@ module.exports = {
             "io"
         );
 
-        let USER = await UsersManager.getUser(member.id);
-        if(USER === null)
+        if(!member.user.bot)
         {
-            if(global.debug) console.log("> User not found, creating new user");
-            USER = await UsersManager.addUser({id: member.id, name: "", surname: "", email: "", password: "", status: -1, data: "{}"});
-            if(global.debug) console.log("> User created");
-            // TODO : Make the welcome form with form ID
-            await UserGuildStatusManager.addUserGuildStatus({id: member.id, guild_id: member.guild.id, status: 0, form_id: null});
-            if(global.debug) console.log("> UserGuildStatus created");
-        } else {
-            if(global.debug) console.log("> User found");
-            await UserGuildStatusManager.addUserGuildStatus({id: member.id, guild_id: member.guild.id, status: 0, form_id: null});
-            if(global.debug) console.log("> UserGuildStatus updated");
+            const userForms = await FormsManager.searchForms(member.id, member.guild.id);
+            if (userForms !== null) {
+                for (const form_id of userForms) await FormsManager.deleteForm(form_id);
+            }
+            // TODO: Need to make this function returns the channel obj to get its id (or fetch it)
+            const channel = createChannel(member.guild, member.user);
+
+            let USER = await UsersManager.getUser(member.id);
+            if(USER === null)
+            {
+                if(global.debug) console.log("> User not found, creating new user");
+                // User
+                USER = await UsersManager.addUser({id: member.id, name: "", surname: "", email: "", password: "", status: -1, data: "{}"});
+                if(global.debug) console.log("> User created");
+                // Form
+                const form = await FormsManager.addForm({user_id: member.id, guild_id: member.guild.id, channel_id: channel.id, status: 0});
+                await form.generateVerificationCode();
+                // User Guild Status
+                await UserGuildStatusManager.addUserGuildStatus({id: member.id, guild_id: member.guild.id, status: 0, form_id: null});
+                if(global.debug) console.log("> UserGuildStatus created");
+            } else {
+                if(global.debug) console.log("> User found");
+                await UserGuildStatusManager.addUserGuildStatus({id: member.id, guild_id: member.guild.id, status: 0, form_id: null});
+                if(global.debug) console.log("> UserGuildStatus updated");
+            }
         }
     }
 }
