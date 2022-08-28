aa6be743d316db8ef0da4e81f05b39c5984074bd
Sigeth <52868615+Sigeth@users.noreply.github.com>
2022-08-28 17:38:44 +0200

    refactor: removed getButtonsFromJSOn from initial file

--- bot/forms/formManager.js
+++ bot/forms/formManager.js
@@ -1,24 +1,6 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { handleWelcomeButtonClick, handleWelcomeFormResponse, handleWelcomeFormMenuResponse } = require("./welcomeForm/welcomeForm.js");
 
-function getButtonsFromJSON(json, response) {
-    let buttons = new Array();
-    for (const value of Object.values(json)) {
-        buttons.push(new ButtonBuilder()
-            .setLabel(value.menu.label)
-            .setEmoji(value.menu.emoji)
-            .setCustomId(value.menu.value)
-            .setStyle(ButtonStyle.Primary)
-        );
-    }
-
-    const actionRow = [new ActionRowBuilder({components: buttons})];
-
-    return actionRow;
-}
-
 module.exports = {
-    getButtonsFromJSON,
     handleButtonClickForm(interaction) {
         if (interaction.customId.toLowerCase().includes("welcome")) {
             handleWelcomeButtonClick(interaction);
