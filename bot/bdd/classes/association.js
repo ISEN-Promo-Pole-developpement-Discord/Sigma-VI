b24aef240c95810abd40bd9e69e82d8be72e414d
brandy223 <brandan.legall@gmail.com>
2022-08-29 05:46:26 +0900

    feat: associations class & manager

---
+++ bot/bdd/classes/association.js
@@ -0,0 +1,54 @@
+class Association
+{
+    constructor(asso_id) {
+        this.asso_id = asso_id;
+    }
+
+    // GETTERS
+    async getName()
+    {
+        const connection = global.sqlConnection;
+        const [rows] = await connection(
+            "SELECT name FROM association WHERE asso_id = ?", [this.asso_id]
+        );
+        return rows[0].name;
+    }
+
+    async getDescription() {
+        const connection = global.sqlConnection;
+        const [rows] = await connection(
+            "SELECT description FROM association WHERE asso_id = ?", [this.asso_id]
+        );
+        return rows[0].description;
+    }
+
+    async getIcon()
+    {
+        const connection = global.sqlConnection;
+        const [rows] = await connection(
+            "SELECT icon FROM association WHERE asso_id = ?", [this.asso_id]
+        );
+        return rows[0].icon;
+    }
+
+    // SETTERS
+    async setName(name)
+    {
+        const connection = global.sqlConnection;
+        await connection(`UPDATE association SET name = ? WHERE asso_id = ?`, [name, this.asso_id]);
+    }
+
+    async setDescription(description)
+    {
+        const connection = global.sqlConnection;
+        await connection(`UPDATE association SET description = ? WHERE asso_id = ?`, [description, this.asso_id]);
+    }
+
+    async setIcon(icon)
+    {
+        const connection = global.sqlConnection;
+        await connection(`UPDATE association SET icon = ? WHERE asso_id = ?`, [icon, this.asso_id]);
+    }
+}
+
+module.exports = {Association};
---
+++ bot/bdd/classes/associationsManager.js
@@ -0,0 +1,23 @@
+const Association = require('./association');
+
+class AssociationsManager
+{
+    static async getAssociations(name)
+    {
+        const connection = global.sqlConnection;
+        const query = "SELECT * FROM association WHERE name = ?";
+        const data = await connection(query, name);
+        if (data.length === 0) return null;
+        else
+            return new Association(data[0].asso_id);
+    }
+
+    static async addAssociation(association)
+    {
+        const connection = global.sqlConnection;
+        const query = "INSERT INTO association (name, description, icon) VALUES (?, ?, ?)";
+        const values = [association.name, association.description, association.icon];
+        await connection(query, values);
+        return await this.getAssociations(association.name);
+    }
+}
