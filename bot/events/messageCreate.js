6f55e18f7b47dc304f27e5a9ce1535b3b7f08f34
Tom Bourjala <tom.bourjala@gmail.com>
2022-08-30 15:04:30 +0200

    fix: (delete) removed all debug commands

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -12,51 +12,6 @@ module.exports = {
          * @returns {Promise<void>}
          */
 
-        //Commande SigmaDeleteChannel :
-        if (message.content.split(` `)[0].toLowerCase() === 'sigmadeletechannel') {
-            message.guild.channels.fetch()
-                .then(channels => channels.forEach((entry, snowflake) => {
-                    if (entry.name) {
-                        if (entry.name === message.content.split(` `)[1]) {
-                            deleteChannel(message.guild, entry.name);
-                        }
-                    }
-                }
-                )
-                )
-            deleteChannel(message.guild, message.channel);
-        }
-        //Commande SigmaCreateChannel :  
-        if (message.content.split(` `)[0].toLowerCase() === 'sigmacreatechannel') {
-                createChannel(message.guild, message.author, message.content.split(` `)[1],message.content.split(` `)[2]);
-            }
-        //Commande SigmaFetchChannel :
-        if (message.content.split(` `)[0].toLowerCase() === 'sigmafetchchannel') {
-            if (!message.content.split(` `)[1]) {
-                fetchChannels(message.channel);
-            }
-            else {
-                fetchChannels(message.channel, message.content.split(` `)[1]);
-            }
-
-        }
-
-        //Commande SigmaClearChannel :
-        if (message.content.split(` `)[0].toLowerCase() === 'sigmaclearchannel') {
-            clearChannel(message.channel)
-        }
-
-        if (message.content.split(` `)[0].toLowerCase() === `sigmacreatethread`) {
-            if (message.content.split(` `)[1]) {
-                createThread(message.channel, message.content.split(` `)[1],message,message.author);   
-            }
-
-        }
-
-        if(message.content.split(` `)[0].toLowerCase() === 'sigmaterminatorchannel') {
-            terminatorChannels(message.guild,message.content.split(` `)[1]);
-            }    
-        
         if (message.author.bot) return;
         if (!message.content) return;
         launchRequestProcessing(message, global.client);
