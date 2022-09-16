1c4e78f0ad47a260f0e27b2ba76493a2c519ca9f
Tom Bourjala <tom.bourjala@gmail.com>
2022-09-16 09:54:18 +0200

    fix: patched meteo placeholder

--- bot/requests/modules/meteo/mod.js
+++ bot/requests/modules/meteo/mod.js
@@ -3,7 +3,7 @@ const {Request} = require("../../requestClass");
 function moduleProcess(request){
     request.notifyStart();
     console.log("[METEO REQUEST] "+request.message.author.tag+" : "+request.message.content);
-    request.notifyEnd("Le module météo n'est pas encore actif.\n> " + request.content + "\n> " + request.SRWF());
+    request.notifyEnd("Le module météo n'est pas encore actif.");
 }
 
 module.exports = moduleProcess;
