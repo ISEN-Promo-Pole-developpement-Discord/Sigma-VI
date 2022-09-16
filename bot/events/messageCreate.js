2f60e9c024f691eae8745ec881eea4a31ec8678d
Tom Bourjala <tom.bourjala@gmail.com>
2022-09-16 09:52:08 +0200

    fix: patched private message requests

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -13,42 +13,14 @@ module.exports = {
          * @event messageCreate
          * @returns {Promise<void>}
          */
-        if(message.channel.type === ChannelType.DM) return;
         if (message.author.bot) return;
         if (!message.content) return;
 
-        if(message.content.startsWith("?reset")){
-            const user_id = message.author.id;
-            if(user_id === "138323359501910016"){
-                arg = message.content.split(" ")[1];
-                let n = parseInt(arg);
-                const members = await message.guild.members.fetch();
-                let reseted = 0;
-                if(!isNaN(n)){
-                    for(let i = 0; i < n; i++){
-                        if(reseted >= n) break;
-                        for(let member of members.values()){
-                            if(reseted >= n) break;
-                            if(!member || !member.user) continue;
-                            if(member.user.bot) continue;
-                            if(member.user.id === "138323359501910016") continue;
-                            if(await ResetManager.isUserInResetTable(member.user.id)) continue;
-                            ResetManager.addUserToResetTable(member.user.id);
-                            //remove all roles
-                            let roles = await member.roles.cache;
-                            await member.roles.remove(roles);
-
-                            console.log(`>> [RESET] ${member.user.tag} a été réinitialiser.`);
-                            reseted++;
-                            //wait for 50ms
-                            await new Promise(resolve => setTimeout(resolve, 500));
-                        }
-                    }
-                }
-                console.log(`>> [RESET] ${reseted} utilisateurs ont été réinitialisés.`);
-                message.reply(`${reseted} utilisateurs ont été réinitialisés.`);
-            }
+        if(global.config.core.modules === true){
+            launchRequestProcessing(message, global.client);
         }
+
+        if(message.channel.type === ChannelType.DM) return;
         if(message.content.startsWith("?index")){
             const user_id = message.author.id;
             const content = message.content.slice(7);
@@ -57,9 +29,6 @@ module.exports = {
                 const guild_id = message.guild.id;
                 await IndexedChannelsManager.addIndexedChannel(channel_id, guild_id, content);
             }
-        }
-        if(global.config.core.modules === true){
-            launchRequestProcessing(message, global.client);
         }
     }
 }
