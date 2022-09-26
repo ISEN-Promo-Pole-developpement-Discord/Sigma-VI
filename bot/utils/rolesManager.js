aba120c817faf8025dc2618634c1c9fa882544ba
Tom Bourjala <tom.bourjala@gmail.com>
2022-09-26 11:04:48 +0200

    fix: removed roleManager logs

--- bot/utils/rolesManager.js
+++ bot/utils/rolesManager.js
@@ -16,29 +16,22 @@ const config = require("../config-core.json");
                 var guildRoleString = guildRole.name.toLowerCase().replace(/ /g, "");
                 if (guildRoleString === targetRoleString)
                 {
-                    if(global.debug) console.log("> Role found: " + guildRole.name);
                     // typeOfAction === 0 => add role
                     if (!typeOfAction)
                     {
                         if (!(member.roles.cache.some(r => r.name === guildRole.name))) {
                             rolesPending.push(member.roles.add(guildRole));
-                        } else
-                            if(global.debug) console.log("> Role already assigned");
+                        }
                     }
                     // typeOfAction === 1 => remove role
                     else
                     {
                         if (member.roles.cache.some(r => r.name === guildRole.name)) {
                             rolesPending.push(member.roles.remove(guildRole));
-                        } else
-                            if(global.debug) console.log("> Role already removed");
+                        }
                     }
                     roleFound = true;
                 }
-            }
-            if (!roleFound)
-            {
-                if(global.debug) console.log("> Role not found: " + role);
             }
         }
     }
@@ -50,12 +43,10 @@ async function assignVerifiedRole(user, guild)
     let roleName = config.verifiedRoleName;
     if (guild.roles.cache.find(r => r.name === roleName))
     {
-        if (global.debug) console.log("> Role found");
         await guild.members.cache.get(user.id).roles.add(guild.roles.cache.find(r => r.name === roleName))
     }
     else
     {
-        if (global.debug) console.log("> Role not found");
         let verifiedRole = await guild.roles.create()
             .then(console.log("> Verified Role created"))
             .catch(console.error);
