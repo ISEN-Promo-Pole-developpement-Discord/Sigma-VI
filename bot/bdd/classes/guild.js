048e61d72b30bd02043130bed7794d6af31324d7
Tom Bourjala <tom.bourjala@gmail.com>
2022-10-13 09:42:19 +0200

    refactor: updated guilds classes to new standart

--- bot/bdd/classes/guild.js
+++ bot/bdd/classes/guild.js
@@ -10,6 +10,12 @@ class Guild
         this.guild_id = guild_id;
     }
 
+    /**
+     * Get the guild's config
+     * @returns {Promise<object>} The guild's config
+     * @deprecated
+     * @async
+     */
     async getConfig() {
         const connection = global.sqlConnection;
         const [rows] = await connection(
@@ -18,8 +24,17 @@ class Guild
         return rows.config;
     }
 
+    /**
+     * Set the guild's config
+     * @param {object} config - The guild's config
+     * @deprecated
+     * @async
+     * @returns {Promise<void>}
+     */
     async setConfig(config)
     {
+        if(typeof config !== "object")
+            throw new TypeError("config must be an object");
         const connection = global.sqlConnection;
         await connection(`UPDATE guild SET config = ? WHERE guild_id = ?`, [config, this.guild_id]);
     }
--- bot/bdd/classes/guildsManager.js
+++ bot/bdd/classes/guildsManager.js
@@ -8,8 +8,18 @@ const { Guild } = require("./guild.js");
  */
 class GuildsManager
 {
+    /**
+     * Get a guild from its ID
+     * @param {string|number} guild_id The guild's ID
+     * @returns {Guild} The guild
+     * @deprecated
+     * @static
+     * @async
+     */
     static async getGuild(guild_id)
     {
+        if(typeof guild_id !== "string" && typeof guild_id !== "number")
+            throw new TypeError("guild_id must be a string or a number");
         const connection = global.sqlConnection;
         const query = "SELECT * FROM guild WHERE guild_id = ?";
         const values = [guild_id];
@@ -20,8 +30,19 @@ class GuildsManager
             return new Guild(guild_id);
     }
 
+    /**
+     * Add a guild to the database
+     * @param {Guild} guild The guild to add
+     * @deprecated
+     * @static
+     * @async
+     * @returns {Promise<Guild>} The guild
+     * @todo switch to guild_id
+     */
     static async addGuild(guild)
     {
+        if (!(guild instanceof Guild))
+            throw new TypeError("guild must be a Guild");
         if (await this.getGuild(guild.id) !== null) return;
         const connection = global.sqlConnection;
         const query = "INSERT INTO guild (guild_id, config) VALUES(?, ?)";
@@ -31,8 +52,18 @@ class GuildsManager
         return new Guild(guild.id);
     }
 
+    /**
+     * Remove a guild from the database
+     * @param {string|number} guild_id The guild's ID
+     * @deprecated
+     * @static
+     * @async
+     * @returns {Promise<void>}
+     */
     static async deleteGuild(guild_id)
     {
+        if(typeof guild_id !== "string" && typeof guild_id !== "number")
+            throw new TypeError("guild_id must be a string or a number");
         if (await this.getGuild(guild_id) === null) return;
         const connection = global.sqlConnection;
         const query = "DELETE FROM guild WHERE guild_id = ?";
